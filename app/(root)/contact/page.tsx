761e49f1513d6b012971379721b918bf0542a618
Sherzod-prog <sherzodnishonov@gmail.com>
2025-06-27 06:09:51 +0500

    contact page

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -3,7 +3,9 @@ const ContactPage = () => {
   return (
     <div className="min-h-screen bg-background ">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="font-heading text-3xl font-bold mb-4">Contact Us</h1>
+        <h1 className="font-heading text-3xl font-bold mb-4 text-center">
+          Contact Us
+        </h1>
         <p className="text-muted-foreground mb-6">
           Get in touch with our BMW dealership team
         </p>
